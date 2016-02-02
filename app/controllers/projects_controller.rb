dc91be246533668de3aaf32b00c5c727853c533f
Uriel7 <j.urielsz@gmail.com>
2016-02-02 11:41:36 -0600

    Add Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,31 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project, notice: "Proyecto guardado"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+  
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
   end
 end
